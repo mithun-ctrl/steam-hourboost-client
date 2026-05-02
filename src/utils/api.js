308767c9d227ce492c0e802ee6afaf8a83021d5c
mithun-ctrl <itsmithun01@gmail.com>
2026-05-02 17:29:21 +0530

    Game search by name and steam guard idling added

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://steam-hour-boost-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
